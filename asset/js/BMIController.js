d0e447abbddde84d7bb693c5e583391984f7f825
juliocezah <julim.cdo@gmail.com>
2015-09-15 02:36:40 +0000

    Add Result-Description

--- asset/js/BMIController.js
+++ asset/js/BMIController.js
@@ -18,16 +18,33 @@ var BMIController = {
 			weight = parseFloat(form.weight.value),
 			height = parseFloat(form.height.value),
 			result = 0;
+			result2 = "";
 		
-		result = BMIService.getIndex(weight, height);
+		var callback = function(result){
+			BMIController.showResult(result);
+		};
 		
-		BMIController.showResult(result);
+		BMIService.getIndex(weight, height, callback);
+		
+		var callback2 = function(result2){
+			BMIController.showDescription(result2);
+		};
+		
+		BMIService.getDescription(callback2);
+		
 	},
 	
 	showResult: function(result) {
 		var spanResult = document.querySelector('.result');
 		spanResult.innerHTML = result.toFixed(2);
 	},
+	
+	showDescription: function(result2){
+		
+		var descriptionResult = document.querySelector('.result2');
+		descriptionResult.innerHTML = result2;
+		
+	}
 
 };
 
--- asset/js/BMIService.js
+++ asset/js/BMIService.js
@@ -1,15 +1,43 @@
 var BMIService = {
 
-	getIndex: function(weight, height) {
-		if(typeof(weight) === 'number' && typeof(height) === 'number') {
-			return weight / (height * height);
-		}
-		return null;
+	getIndex: function(weight, height, callback) {
+		$.ajax({
+			url: 'services/bmi.action.php',
+			data: {'weight': weight, 'height': height},
+			success: function(result){
+				callback(parseFloat (result));
+			},
+			error: function(){
+				callback(null);			}
+		});
 	},
 	
 	getDescription: function (index) {
-		//TODO to be implemented
-		return "";
+	var text = " ";
+		
+		if (index < 17){
+			text = "ATENÇÃO!!! Você está MUITO abaixo do peso.";
+		}
+		else if (index >= 17 && index <= 18,49){
+			text = "ATENÇÃO!!! Você está abaixo do peso.";
+		}
+		else if (index >= 18,5 && index <= 24,99){
+			text = "Você está no seu peso ideal.";
+		}
+		else if (index >= 25 && index <= 29,99){
+			text = "ATENÇÃO!!! Você está acima do peso.";
+		}
+		else if (index >= 30 && index <= 34,99){
+			text = "ATENÇÃO!!! Obesidade de grau I";
+		}
+		else if (index >= 35 && index <= 39,99){
+			text = "ATENÇÃO!!! Obesidade de grau II";
+		}
+		else if (index > 40){
+			text = "ATENÇÃO!!! Obesidade de grau III";
+		}
+		
+		return text;
 	}
 	
 };
